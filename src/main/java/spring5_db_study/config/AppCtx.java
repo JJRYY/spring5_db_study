9f03031060ef9cf89b5dd7e72c7347dc21800c13
Ryong <kjr2436@gmail.com>
2021-05-18 11:47:40 +0900

    set component

--- src/main/java/spring5_db_study/config/AppCtx.java
+++ src/main/java/spring5_db_study/config/AppCtx.java
@@ -44,52 +44,5 @@ public class AppCtx {
 		return tm;
 	}
 	
-	@Bean
-	public MemberDao memberDao() {
-		return new MemberDao();
-	}
-	
-	@Bean
-	public MemberRegisterService memberRegSvc() {
-		return new MemberRegisterService(/* memberDao() */);
-	}
-	
-	@Bean
-	public ChangePasswordService changePwdSvc() {
-		ChangePasswordService pwdSvc = new ChangePasswordService();
-//		pwdSvc.setMemberDao(memberDao());
-		return pwdSvc;
-	}
-	
-//	@Bean
-//	public MemberSelectAllService selectAllSvc() {
-//		return new MemberSelectAllService(memberDao());
-//	}
-	
-	@Bean
-	public MemberPrinter memberPrinter() {
-		return new MemberPrinter();
-	}
-	
-	@Bean
-	public MemberListPrinter listPrinter() {
-		return new MemberListPrinter(/* memberDao(), memberPrinter() */);
-	}
-	
-	@Bean
-	public MemberInfoPrinter infoPrinter() {
-		MemberInfoPrinter infoPrinter = new MemberInfoPrinter();
-//		infoPrinter.setMemDao(memberDao());
-//		infoPrinter.setPrinter(memberPrinter());
-		return infoPrinter;
-	}
-	
-	@Bean
-	public VersionPrinter versionPrinter() {
-		VersionPrinter versionPrinter = new VersionPrinter();
-		versionPrinter.setMajorVersion(5);
-		versionPrinter.setMinorVersion(0);
-		return versionPrinter;
-	}
-	
+
 }
